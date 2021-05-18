857d332d77b9ad0eda43dfbc7f1b65bea1ded3e1
muzica1155@outlook.com <muzica1155@outlook.com>
2021-05-18 12:54:27 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   //entry point
@@ -9,8 +10,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-    String name = "hello";
+    // int days = 30;
+    // String name = "hello";
     // double pi = 3.14;
     // bool isMale = true;
     // num temp = 30.5; //name can accept int and double
@@ -19,7 +20,21 @@ class MyApp extends StatelessWidget {
     // final   // final is a const but can be modify the value difference is u can Arithmetic operation
 
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          primarySwatch: Colors.pink), //primarySwatch collection of color
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home", // if u want to use by default home page
+      routes: {
+        "/": (context) => LoginPage(), // "/" but default HomePage(),
+        "/home": (context) => HomePage(),
+
+        ///LoginPage() is define as object as use new keywords to define
+        "/Login": (context) => LoginPage(),
+      }, // 4 properties
     );
   }
 }
